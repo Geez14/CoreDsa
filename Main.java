4e6a1a51ec4701ce4fb511136fb5a9855e05765d
aster14 <91541128+Geez14@users.noreply.github.com>
2023-08-19 04:04:48 +0530

    some little changes

--- Main.java
+++ Main.java
@@ -2,5 +2,9 @@ class Main {
     public static void main(String...args)
     {
         int[] aste = new int[3];
+        aste[0] = 3;
+        aste[1] = 34;
+        aste[2] = 45;
+        aste[3] = 243;
     }
 }
